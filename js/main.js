f68a7a4a842e7456b4c422eb6dbc4ae7ce88e411
seanot <sean@kingfieldcapital.com>
2015-05-08 11:12:02 -0500

    added a sitemap file

--- js/main.js
+++ js/main.js
@@ -1,52 +1,23 @@
-// (function() {
-//     var headroom = new Headroom(document.querySelector("#header"), {
-//         tolerance: 5,
-//         offset : 500,
-//         classes: {
-//           initial: "animated",
-//           pinned: "slideDown",
-//           unpinned: "slideUp"
-//         }
-//     });
-//     headroom.init();
-
-//     // var bttHeadroom = new Headroom(document.getElementById("btt"), {
-//     //     tolerance : 0,
-//     //     offset : 500,
-//     //     classes : {
-//     //         initial : "slide",
-//     //         pinned : "slide--reset",
-//     //         unpinned : "slide--down"
-//     //     }
-//     // });
-//     bttHeadroom.init();
-// }());
-
-jQuery(document).ready(function($) {
-
-    $(".site-header").headroom({
-        // scroll tolerance in px before state changes
-        "tolerance": 5,
-
-        // vertical offset in px before element is first unpinned
-        "offset": 250, /* set this to height of your header */
-
-        "classes": {
-            // when element is initialised
-            "initial": "animated",
-
-            // when scrolling up
-            "pinned": "slideDown",
-
-            // when scrolling down
-            "unpinned": "slideUp",
-
-            // when above offset
-            "top": "headroom--top",
-
-            // when below offset
-            "notTop": "headroom--not-top"
+(function() {
+    var header = new Headroom(document.querySelector("#header"), {
+        tolerance: 5,
+        offset : 65,
+        classes: {
+          initial: "headroom",
+          pinned: "headroom--pinned",
+          unpinned: "headroom--unpinned"
         }
     });
+    header.init();
 
-});
+    var bttHeadroom = new Headroom(document.getElementById("btt"), {
+        tolerance : 0,
+        offset : 500,
+        classes : {
+            initial : "slide",
+            pinned : "slide--reset",
+            unpinned : "slide--down"
+        }
+    });
+    bttHeadroom.init();
+}());
